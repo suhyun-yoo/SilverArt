9c730b43ed5293c06663a06f2e7f7eaf5f5e86a4
suhyun-yoo <wau3384@gmail.com>
2024-04-06 14:39:00 +0900

    로그인 모달 수정

--- Client/src/compontents/LoginModal.jsx
+++ Client/src/compontents/LoginModal.jsx
@@ -8,6 +8,12 @@ export default function LoginModal({setActive, setShowModal}) {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
+  const handleIdChange = (e) => {
+    setId(e.target.value); // ID 상태 업데이트
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value); // Password 상태 업데이트
+  };
   const handleLogin = () => {
     axios.post('http://localhost:5000/admin/login', { id, password })
       .then(res => {
@@ -30,19 +36,19 @@ export default function LoginModal({setActive, setShowModal}) {
   return (
     <div className="login-wrap">
       <div className="txtBox">
-      <div className="close" onClick={() => setShowModal(false)}>
-        <svg viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M55.5564 55.6693L24.4437 24.5566" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" />
-          <path d="M24.4436 55.6693L55.5563 24.5566" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-      </div>
+        <div className="close" onClick={() => setShowModal(false)}>
+          <svg viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M55.5564 55.6693L24.4437 24.5566" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M24.4436 55.6693L55.5563 24.5566" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </div>
         <div className="inputBox">
           <label htmlFor="id"> ID </label>
-          <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
+          <input type="text" id="id" value={id} onChange={handleIdChange} autoComplete="off" />
         </div>
         <div className="inputBox">
           <label htmlFor="pw">Password</label>
-          <input type="password" id="pw" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" id="pw" value={password} onChange={handlePasswordChange} />
         </div>
         <button onClick={handleLogin}> Login </button>
       </div>
